8cf27b6e9476f2a1255fdf70abe589e638719999
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-01-15 19:26:11 +0530

    tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:"jit",
+  content: ["./public/index.html","./src/*.{htmk,js,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
